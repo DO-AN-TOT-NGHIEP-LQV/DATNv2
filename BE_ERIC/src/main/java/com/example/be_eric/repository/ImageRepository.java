7492f3e581fc1b9b4cd06fb641d61c379ff05bf4
LeQuocViet2001 <vietdat2012@gmail.com>
2023-07-01 12:56:03 +0700

    fe

--- BE_ERIC/src/main/java/com/example/be_eric/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.be_eric.repository;
-
-import com.example.be_eric.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
